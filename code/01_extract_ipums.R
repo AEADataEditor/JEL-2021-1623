aede39caf1e1d546f1afa075ac34ea512b3b235b
Lars Vilhuber <lars@vilhuber.com>
2022-03-08 08:32:20 -0500

    Updated run

--- code/01_extract_ipums.R
+++ code/01_extract_ipums.R
@@ -2,12 +2,19 @@
 Sys.getenv("IPUMS_API_KEY")
 # See https://tech.popdata.org/ipumsr/dev/articles/ipums-api.html#sharing-an-extract-definition-1 for more details.
 library(ipumsr)
+extract.spec <- file.path("data","author-extract.Rds")
 # Recover the extract specification
 clone_extract <- define_extract_from_json(
                      file.path("code","author-extract.json"), 
                      "usa")
-# Submit the extract
-submitted_extract <- submit_extract(clone_extract)
+
+if ( file.exists(extract.spec)) {
+  submitted_extract <- readRDS(extract.spec)
+} else {
+  # Submit the extract
+  submitted_extract <- submit_extract(clone_extract)
+  saveRDS(submitted_extract,extract.spec)
+}
 # Provide summary info on the submitted extract
 print(submitted_extract)
 # Initial status
@@ -16,4 +23,5 @@ get_extract_info(submitted_extract)$status
 downloadable_extract <- wait_for_extract(submitted_extract)
 # when that finishes, we can save it.
 path_to_ddi_file <- download_extract(downloadable_extract,
-                                     download_dir=file.path("data"))
+                                     download_dir=file.path("data"),
+                                     overwrite = TRUE)
